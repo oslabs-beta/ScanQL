49017dd0cfcb8d978840ae5b016289251dbfaed5
Sam Heck <heck.sam@gmail.com>
2023-08-17 16:34:52 -0700

    create static asset route in server/server.ts. add an import to server/server.ts to allow use of '__dirname' in es module scope

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const express = require('express');
-// const path = require('path');
+
 import express, { Request, Response, NextFunction } from 'express';
-// import path from 'path';
+import path from 'path';
+// allows the use __dirname in es module scope
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 import pgRoute from './routes/pgRoute.js';
 import authRoute from './routes/authRoute.js';
 
-// app.use(express.static(path.join(__dirname, '../src/assets')));
+app.use(express.static(path.join(__dirname, '../src/assets')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // routes
 app.use('/api/pg', pgRoute);
-  
 app.use('/auth', authRoute);
  
 // unknown routre handler
