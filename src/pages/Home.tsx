80055e7226882f4631fe1005945b6a7d697b5b56
danykdev <108373212+danykdev@users.noreply.github.com>
2023-08-17 19:57:09 -0700

    included additional routes to login, signup, home. updating dashboard navbar and created login page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import NavBar from '../components/layout/NavBar';
+
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      {/* <h1>Home</h1> */}
+      <NavBar />
+    </div>
+  )
+}
+
+export default Home;
