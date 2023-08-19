a475931540d7653ed931b39522c40c478275f05e
danykdev <108373212+danykdev@users.noreply.github.com>
2023-08-19 11:23:17 -0700

    reorganized router paths, installed radix dialog, updated db connection modal, added layout file

--- src/components/ui/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu.tsx
@@ -34,8 +34,8 @@ const DropdownMenuIcon = () => {
           className="min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
           sideOffset={5}
         >
-          <DropdownMenu.Item onClick={handleConnectToDB} className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
-            Connect to Database{''} 
+          <DropdownMenu.Item onClick={openConnectDB} className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1">
+            Connect to Database{''}
             <div className="ml-auto pl-[20px] text-mauve11 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8">
               ⌘+D
             </div>
@@ -47,7 +47,7 @@ const DropdownMenuIcon = () => {
               ⌘+H
             </div>
           </DropdownMenu.Item>
-          
+
 
           <DropdownMenu.Sub>
             <DropdownMenu.SubTrigger className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[state=open]:bg-violet4 data-[state=open]:text-violet11 data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 data-[highlighted]:data-[state=open]:bg-violet9 data-[highlighted]:data-[state=open]:text-violet1">
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -144,6 +144,8 @@ const Dashboard: React.FC = () => {
           </div>
         </div>
       </div>
+
+      
     </div>
   );
 }
