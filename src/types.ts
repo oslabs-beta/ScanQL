409600c43517de8b6998d89d4a5c59f144bcc0fb
YahyaT95 <yahyaex2022@outlook.com>
2023-08-23 01:36:59 -0400

    Save changes before switching branches

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,9 @@
 export type RFState = {
-    edges: any[];
-    setEdges: (eds: any) => void;
-    nodes: any[];
-    setNodes: (nds: any) => void;
-    onNodesChange: (changes: any) => void;
-    onEdgesChange: (changes: any) => void;
-    onConnect: (connection: any) => void;
+  edges: any[];
+  setEdges: (eds: any) => void;
+  nodes: any[];
+  setNodes: (nds: any) => void;
+  onNodesChange: (changes: any) => void;
+  onEdgesChange: (changes: any) => void;
+  onConnect: (connection: any) => void;
 };
-import { TableInfo } from './store/appStore'
-
-export type RowsInfo = {
-    tableName: string;
-    numberOfRows: number;
-}
-
-export type RowsInfoArray = RowsInfo[];
-
-export type indexInfo = {
-    tableName: string;
-    numberOfIndexes: number;
-}
-
-export type indexTableArray = indexInfo[];
