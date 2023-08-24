cf0e1499ce865f1204690dca19d7e63995674aa7
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-08-24 16:29:56 -0500

    create edges in RF to connect tables

--- src/components/ReactFlow/createEdges.ts
+++ src/components/ReactFlow/createEdges.ts
@@ -1,20 +1,25 @@
+import { table } from "console";
 import { Edge } from "reactflow";
 
 export default function createEdges(ERDdata: any) {
     // ERDdata is an object of tables
-    const initialEdges:Edge[] =  [];
-
-    for (const tables in ERDdata){
-        if (ERDdata[tables].primary_key === true){
-            const target = ERDdata[tables];
-
-            ERDdata[tables].foreign_tables.forEach((element: string) => {
-                const source = element
-                initialEdges.push(
-                {id:`${source}-${target}`, source: source, target:target}
-            )
+  const initialEdges:Edge[] =  [];
+  for (const tableName in ERDdata){
+    const ColumnData = ERDdata[tableName]
+    for (const columns in ColumnData){
+        if (ColumnData[columns].primary_key === 'true'){
+            const target = tableName;
+            ColumnData[columns].foreign_tables.forEach((element: string) => {
+              const source = element;
+              initialEdges.push({
+                id: `${source}-${target}`,
+                source: source,
+                target: target,
+              });
             });
         }
     }
-    return initialEdges;
+  }
+  // console.log(initialEdges)
+  return initialEdges;
 };  
