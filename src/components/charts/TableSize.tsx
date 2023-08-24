83776c42c06c76e8b01301522f5b35ea1a53b87c
Sam Heck <heck.sam@gmail.com>
2023-08-23 20:28:08 -0700

    complete TableSize component, convert recieved data to kb using custom helper function.

--- src/components/charts/TableSize.tsx
+++ src/components/charts/TableSize.tsx
@@ -1,17 +1,30 @@
 import { Bar } from 'react-chartjs-2';
 import useAppStore from '../../store/appStore';
 
+interface Table {
+  diskSize: String;
+  rowSize: String;
+}
+
+// only converts to kb. could be refactored to loop through all the size units and find the one that is the most common, than proivde different scenarios for converting to each unit (kb, mb, gb); use a cache to store quantities of found unit tied to the unit as the key. 
+const toNumInKB = (size: String): number => { 
+  let num = '';
+  for (const char of size) {
+    if (/[0-9]/.test(char)) num += char;
+    else break;
+  }
+  // check if bytes, convert accordingly and return
+  if (size.toLowerCase().includes('bytes')) return parseInt(num) / 1000;
+  // check if mB, convert accordingly and return
+  if (size.toLowerCase().includes('mb')) return parseInt(num) * 1000;
+  // return num if already kb
+  return parseInt(num);
+}
+
 
 export const TableSize: React.FC = () => {
   const { metricsData } = useAppStore();
-  console.log(metricsData.dbSizeMetrics.tableSizes);
-  const tablesArray = Object.values(metricsData.dbSizeMetrics.tableSizes);
-  const diskSize = tablesArray.map(table => {
-    // return disk size in kb
-  })
-  const rowSize = tablesArray.map(table => {
-    // return rowSize in kb
-  })
+  const tablesArray: Table[] = Object.values(metricsData.dbSizeMetrics.tableSizes);
 
   const options = {
     responsive: true,
@@ -21,7 +34,7 @@ export const TableSize: React.FC = () => {
       },
       title: {
         display: true,
-        text: `Planning Execution Times - `,
+        text: 'Table Sizes (kb)',
         color: '#ffffffc8'
       },
     },
@@ -31,21 +44,15 @@ export const TableSize: React.FC = () => {
     labels: Object.keys(metricsData.dbSizeMetrics.tableSizes),
     datasets: [
       {
-        label: 'Planning Time',
-        data: [],
+        label: 'Disk Size',
+        data: tablesArray.map(table => toNumInKB(table.diskSize)),
         backgroundColor: 'rgba(107, 99, 255, 0.5)',
         scaleFontColor: "#FFFFFF",
       },
       {
-        label: 'Execution Time',
-        data: [],
+        label: 'Row Size',
+        data: tablesArray.map(table => toNumInKB(table.rowSize)),
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        scaleFontColor: "#FFFFFF",
-      },
-      {
-        label: 'Total Time',
-        data: [],
-        backgroundColor: 'rgba(235, 86, 255, 0.2)',
         scaleFontColor: "#FFFFFF",
       },
     ],
