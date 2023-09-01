cc8c70a4f13e7f4280f097409aceb2da6aff0def
Danny Murcia <76888535+dm2800@users.noreply.github.com>
2023-08-31 17:33:39 -0700

    add z layout and cards for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { useEffect } from "react";
 import logo from "../assets/logo-horizontal-v2.png";
+import screenshot1 from "../assets/screenshots/screenshot1.png";
+import screenshot2 from "../assets/screenshots/screenshot1.png";
+import screenshot3 from "../assets/screenshots/screenshot1.png";
 
 import NavBar from "../components/layout/NavBar";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -28,7 +31,10 @@ const Home: React.FC = () => {
                     <p className="home-description-text text-2xl ml-7 text-indigo-900 text-opacity-70">
                         Elegant data visualization for PostgresQL
                     </p>
-                    <Button onClick={()=>loginWithRedirect()} className="mt-10 border border-indigo-900">
+                    <Button
+                        onClick={() => loginWithRedirect()}
+                        className="mt-10 border border-indigo-900 border border-opacity-30 bg-indigo-900 bg-opacity-50 text-gray-200 rounded-lg"
+                    >
                         Get Started
                     </Button>
                 </div>
@@ -37,25 +43,79 @@ const Home: React.FC = () => {
                         <h3 className="text-white text-opacity-80 font-semibold mt-5 text-xl">
                             Speed
                         </h3>
-                        <p className="mt-3 text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p className="mt-3 text-white text-sm">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.{" "}
+                        </p>
                     </div>
                     <div className="home-card">
                         <h3 className="text-white text-opacity-80 font-semibold mt-5 text-xl">
                             Accuracy
                         </h3>
-                        <p className="mt-3 text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p className="mt-3 text-white text-sm">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.{" "}
+                        </p>
                     </div>
                     <div className="home-card">
                         <h3 className="text-white text-opacity-80 font-semibold mt-5 text-xl">
                             Flexibility
                         </h3>
-                        <p className="mt-3 text-sm text-white text-opacity-80">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p className="mt-3 text-sm text-white text-opacity-80">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.{" "}
+                        </p>
                     </div>
                 </div>
-                <div className="home-gif1-section">Gif1</div>
-                <div className="home-gif2-section">Gif2</div>
+                <div className="home-gif1-section">
+                    <div className="home-panel">
+                        <h2 className="mt-10 font-semibold text-3xl text-white text-opacity-80">
+                            Feature 1
+                        </h2>
+                        <p className="mt-10 text-white text-opacity-70">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.
+                        </p>
+                    </div>
+                    <div className="home-panel-img">
+                        <img className="home-demo-img" src={screenshot1}></img>
+                    </div>
+                </div>
+                <div className="home-gif2-section">
+                    <div className="home-panel-img">
+                        <img className="home-demo-img" src={screenshot1}></img>
+                    </div>
+                    <div className="home-panel">
+                        <h2 className="mt-10 font-semibold text-3xl text-white text-opacity-80">
+                            Feature 2
+                        </h2>
+                        <p className="mt-10 text-white text-opacity-70">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div className="home-gif3-section">Gif3</div>
+            <div className="home-gif3-section">
+            <div className="home-panel">
+                        <h2 className="mt-10 font-semibold text-3xl text-white text-opacity-80">
+                            Feature 1
+                        </h2>
+                        <p className="mt-10 text-white text-opacity-70">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.
+                        </p>
+                    </div>
+                    <div className="home-panel-img">
+                        <img className="home-demo-img" src={screenshot1}></img>
+                    </div>
+                </div>
             <Footer></Footer>
         </div>
     );
