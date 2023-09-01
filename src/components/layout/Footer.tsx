f38e2181e9da718e9edf99f5fe04d025a08bad82
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-09-01 17:00:15 -0500

    add layout for about page content

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,35 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <div className="home-footer-section px-60 h-80">
-      <div className="flex justify-evenly flex-end content-end pt-16">
+      <div className="flex justify-evenly flex-end content-end pt-16 ml-40">
+        <div className="">
+          <h3 className="text-xl text-indigo-900 text-opacity-80">
+            Navigation
+          </h3>
+          <ul>
+            <li className="pt-2">
+              <a href="" className="text-slate-300 text-s">
+                Home
+              </a>
+            </li>
+            <li className="pt-2">
+              <a href="" className="text-slate-300 text-s">
+                About Us
+              </a>
+            </li>
+            <li className="pt-2">
+              <a href="" className="text-slate-300 text-s">
+                App
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img
+            src="src/assets/logo-horizontal-v2.png"
+            className="w-auto h-16 mt-6"
+          />
+        </div>
         <div className="">
           <h3 className="text-xl text-indigo-900 text-opacity-80">Community</h3>
           <ul>
@@ -24,29 +52,13 @@ const Footer: React.FC = () => {
             </li>
           </ul>
         </div>
-        <div className="">
-          <h3 className="text-xl text-indigo-900 text-opacity-80">Navigation</h3>
-          <ul>
-            <li className="pt-2">
-              <a href="" className="text-slate-300 text-s">
-                Home
-              </a>
-            </li>
-            <li className="pt-2">
-              <a href="" className="text-slate-300 text-s">
-                About Us
-              </a>
-            </li>
-            <li className="pt-2">
-              <a href="" className="text-slate-300 text-s">
-                App
-              </a>
-            </li>
-          </ul>
+        <div>
+          {/* <img
+            src="src/assets/logo-horizontal-v2.png"
+            className="w-60 h-auto mt-6"
+          /> */}
+          {/* <p className='text-center'>Thank you for visiting <br/></p> */}
         </div>
-        {/* <div>
-          <p></p>
-        </div> */}
       </div>
       <div className="pt-16">
         <div className="border-t py-6">
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,15 +2,286 @@ import Footer from "../components/layout/Footer"
 import NavBar from "../components/layout/NavBar"
 
 const About: React.FC = () => {
-    return (
-        <div>
-            <NavBar/>
-            <div className="h-screen">
-
+  return (
+    <div>
+      <NavBar />
+      <div className="mt-20">
+        <header>
+          <p className="text-center -mb-10">Meet The Current Team</p>
+        </header>
+        <div className="aboutus-container px-80 mt-20">
+          <div className="border-2 border-solid border-black flex gap-32">
+            <div className="flex-column items-center border-2 border-black p-6">
+              <div className="w-52 h-52">
+                {/* image container*/}
+                <img
+                  className="h-52 w-52 object-cover rounded-full"
+                  src="src/assets/devPhotos/Kurt.jpeg"
+                />
+              </div>
+              <div className="">
+                <p className="text-center">Kurt Bulau</p>
+                <p className="text-center -mt-1">Software Engineer</p>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
             </div>
-            <Footer/>
+            <div className="border-2 border-black p-6">
+              <p>Hello</p>
+              <div>
+                <p>General brief about what you like to do any fun facts</p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="aboutus-container px-80 mt-20">
+          <div className="border-2 border-solid border-black flex gap-32">
+            <div className="flex-column items-center border-2 border-black p-6">
+              <div className="w-52 h-52">
+                {/* image container*/}
+                <img
+                  className="h-52 w-52 object-cover rounded-full"
+                  src="src/assets/devPhotos/Kurt.jpeg"
+                />
+              </div>
+              <div className="">
+                <p className="text-center">Kurt Bulau</p>
+                <p className="text-center -mt-1">Software Engineer</p>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="border-2 border-black">
+              <p></p>
+              <p>General brief about what you like to do any fun facts</p>
+            </div>
+          </div>
+        </div>
+        <div className="aboutus-container px-80 mt-20">
+          <div className="border-2 border-solid border-black flex gap-32">
+            <div className="flex-column items-center border-2 border-black p-6">
+              <div className=" pl-1 w-52 h-52">
+                {/* image container*/}
+                <img
+                  className="h-52 w-52 object-cover rounded-full"
+                  src="src/assets/devPhotos/Kurt.jpeg"
+                />
+              </div>
+              <div className="">
+                <p className="text-center">Kurt Bulau</p>
+                <p className="text-center -mt-1">Software Engineer</p>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="border-2 border-black">
+              <p></p>
+              <p>General brief about what you like to do any fun facts</p>
+            </div>
+          </div>
+        </div>
+        <div className="aboutus-container px-80 mt-20">
+          <div className="border-2 border-solid border-black flex gap-32">
+            <div className="flex-column items-center border-2 border-black p-6">
+              <div className=" pl-1 w-52 h-52">
+                {/* image container*/}
+                <img
+                  className="h-52 w-52 object-cover rounded-full"
+                  src="src/assets/devPhotos/Kurt.jpeg"
+                />
+              </div>
+              <div className="">
+                <p className="text-center">Kurt Bulau</p>
+                <p className="text-center -mt-1">Software Engineer</p>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="border-2 border-black">
+              <p></p>
+              <p>General brief about what you like to do any fun facts</p>
+            </div>
+          </div>
+        </div>
+        <div className="aboutus-container px-80 mt-20">
+          <div className="border-2 border-solid border-black flex gap-32">
+            <div className="flex-column items-center border-2 border-black p-6">
+              <div className=" pl-1 w-52 h-52">
+                {/* image container*/}
+                <img
+                  className="h-52 w-52 object-cover rounded-full"
+                  src="src/assets/devPhotos/Kurt.jpeg"
+                />
+              </div>
+              <div className="">
+                <p className="text-center">Kurt Bulau</p>
+                <p className="text-center -mt-1">Software Engineer</p>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="border-2 border-black">
+              <p></p>
+              <p className="">
+                General brief about what you like to do any fun facts
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="contributors-container mt-20">
+          <p className="text-center ">Previous Contributors</p>
+          <div className="grid grid-cols-5 mx-40 my-10">
+            <div className="flex gap-2 m-2">
+              <p>Kurt Bulau</p>
+              <div className="flex gap-2 justify">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="flex gap-2 m-2">
+              <p>Kurt Bulau</p>
+              <div className="flex gap-2 justify">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="flex gap-2 m-2">
+              <p>Kurt Bulau</p>
+              <div className="flex gap-2 justify">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="flex gap-2 m-2">
+              <p>Kurt Bulau</p>
+              <div className="flex gap-2 justify">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="flex gap-2 m-2">
+              <p>Kurt Bulau</p>
+              <div className="flex gap-2 justify">
+                <a href="https://github.com/kbulau">
+                  <img
+                    className="h-5"
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  />
+                </a>
+                <a href="https://www.linkedin.com/in/kurt-bulau/">
+                  <img
+                    className="h-5"
+                    src="https://cdn.onlinewebfonts.com/svg/img_24651.png"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default About
