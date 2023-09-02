2df0c31e0e3570ca0e7ac470dd8be73fd8452fba
Sam Heck <heck.sam@gmail.com>
2023-09-02 16:43:18 -0700

    implement unit testing for ERDView and CustomQueryView components

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/vitest'
+import { mockReactFlow } from './RFSetup'
+
+// call globally to mock missing Web API's and DOM elements needed to render React Flow
+mockReactFlow();
